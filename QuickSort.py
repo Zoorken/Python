2e81f36de1762dfdb4002e8df2d8b50632828849
John Doe <Bent_9_3@hotmail.com>
2015-04-16 21:38:40 +0200

    Call by reference
    Hmm, it seem like there is a difference between how python and C++
    handles memory placec. In c++ you call by reference but Python seems to
    don't use call by reference or call by value.

--- QuickSort.py
+++ QuickSort.py
@@ -1,22 +1,22 @@
 # Hello World program in Python
-theArray = [0,10,4,100,5,60,90,80,100]
+theArray = [3,10,4,100,5,60,90,80,100]
 
 def partion(theArray,start,end):
     pivotPos = start
     i = start + 1
-    while i < end:
+    while i <= end:
         if theArray[pivotPos]>theArray[i]:
-            swapThem(theArray[i],theArray[pivotPos+1])
-            swapThem(theArray[pivotPos],theArray[pivotPos+1])
+            swapThem(i,pivotPos+1)
+            swapThem(pivotPos,pivotPos+1)
             pivotPos += 1
         i += 1
     return pivotPos
     
 
-def swapThem(item1, item2):
-    temp = item1
-    item1 = item2
-    item2 = temp      
+def swapThem(index1, index2):
+    temp = theArray[index1]
+    theArray[index1] = theArray[index2]
+    theArray[index2] = temp      
 
 def quickSort(theArray,start,end):
     if(start < end):
@@ -25,6 +25,6 @@ def quickSort(theArray,start,end):
         quickSort(theArray,pivotPos+1,end)
     
 
-quickSort(theArray,0,len(theArray))
+quickSort(theArray,0,len(theArray)-1)
 
 print (theArray)
